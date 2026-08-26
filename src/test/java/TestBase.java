ff185e66c88b98816885e73151fd0b768adeeacc
SlavaFetisov <fetisov.vn@mail.ru>
2026-08-26 23:39:10 +0700

    update build.gradle and TestBase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,59 +1,40 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.WebDriverConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.MainPage;
 
-import java.util.Map;
-
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.hasWebDriverStarted;
 
 public class TestBase {
-    final static WebDriverConfig webDriverConfig = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
 
     MainPage mainPage = new MainPage();
 
-    @BeforeAll
-    static void setUp() {
-
-        Configuration.baseUrl = webDriverConfig.baseUrl();
-        Configuration.browser = webDriverConfig.browser();
-        Configuration.browserSize = webDriverConfig.browserSize();
-        Configuration.browserVersion = webDriverConfig.browserVersion();
-        Configuration.pageLoadStrategy = "eager";
-
-        if (webDriverConfig.isRemote()) {
-            Configuration.remote = String.valueOf(webDriverConfig.remoteUrl());
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                    "enableVNC", true,
-                    "enableVideo", true
-            ));
-            Configuration.browserCapabilities = capabilities;
-
-        }
-    }
-
     @BeforeEach
-    void beforeEach() {
+    void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        mainPage.openPage();
     }
 
+    @BeforeAll
+    static void setupConfig() {
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://shop.proamk.ru");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+    }
 
     @AfterEach
-    void afterEach() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.addVideo();
-        if (Configuration.browser.equals("chrome")) {
+    void addAttachments() {
+        if (hasWebDriverStarted()) {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
             Attach.browserConsoleLogs();
+            closeWebDriver();
         }
-        closeWebDriver();
     }
 }
